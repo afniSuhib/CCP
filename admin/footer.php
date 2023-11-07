57ae0ffbdaea4994b387020075e28608d6959b28
afniSuhib <124663407+afniSuhib@users.noreply.github.com>
2023-11-07 22:18:23 +0800

    afnicantik

---
+++ admin/footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; <?php echo date('Y');?> Clean Connect Pro | Crafted With <i class ="fa fa-heartbeat"></i> By Afni & Damia
+           </span>
+          </div>
+        </div>
+</footer>
